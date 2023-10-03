242495869202bea5bde9dbc0e451c6ba4c464d82
ilyalomaeff <ilyalomaev146@gmail.com>
2023-10-03 23:56:44 +0300

    Initial Commit

---
+++ IAmRichApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmRichApp
+//
+//  Created by Ilya Lomaev MacBook Pro 13 on 03.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
